6c2c0f8de9a646bac92e43e4c746e9675add1365
Lind <448929489@qq.com>
2021-01-26 23:17:11 +0800

    LogicalOperation

---
+++ zucker-easy-orm-core/src/main/java/org/zucker/ezorm/core/NestConditional.java
@@ -0,0 +1,8 @@
+package org.zucker.ezorm.core;
+
+/**
+ * @auther: lind
+ * @since: 1.0
+ */
+public interface NestConditional<T extends TermTypeConditionalFromBeanSupport> {
+}
---
+++ zucker-easy-orm-core/src/main/java/org/zucker/ezorm/core/TermTypeConditionalSuppport.java
@@ -0,0 +1,24 @@
+package org.zucker.ezorm.core;
+
+import org.zucker.ezorm.core.param.Term;
+
+/**
+ * @auther: lind
+ * @since: 1.0
+ */
+public interface TermTypeConditionalSuppport {
+
+    /**
+     * 条件接收器，用于处理接收到的条件并进行对应的操作如{@link Term.Type#and}
+     *
+     * @param <T>
+     * @param <O>
+     */
+    interface Accepter<T, O> {
+        T accept(String column, String termType, O value);
+    }
+
+    interface SimpleAccepter<T, O> {
+        T accept(String column, O value);
+    }
+}
