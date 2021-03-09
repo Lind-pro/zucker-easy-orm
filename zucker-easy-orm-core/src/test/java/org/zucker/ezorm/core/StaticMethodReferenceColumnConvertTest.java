ebf159c08384c6b54ec7d9699c731ea2641326f2
Lind <448929489@qq.com>
2021-03-09 23:15:26 +0800

    单元测试

---
+++ zucker-easy-orm-core/src/test/java/org/zucker/ezorm/core/StaticMethodReferenceColumnConvertTest.java
@@ -0,0 +1,34 @@
+package org.zucker.ezorm.core;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.Serializable;
+
+/**
+ * @auther: lind
+ * @since: 1.0
+ */
+public class StaticMethodReferenceColumnConvertTest {
+
+    @Test
+    public void testConvertLambdaColumn(){
+        TestClass testClass =new TestClass();
+
+        Assert.assertEquals(MethodReferenceConverter.convertToColumn(TestClass::getName),"name");
+        Assert.assertEquals(MethodReferenceConverter.convertToColumn(TestClass::isEnabled),"enabled");
+
+        Assert.assertEquals(MethodReferenceConverter.convertToColumn(testClass::getName),"name");
+        Assert.assertEquals(MethodReferenceConverter.convertToColumn(testClass::isEnabled),"enabled");
+    }
+
+    @Getter
+    @Setter
+    public static class TestClass implements Serializable{
+        private String name;
+
+        private boolean enabled;
+    }
+}
