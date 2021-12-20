6bb4368aefd0559f7140f204f1e16af3c6425901
Lind <448929489@qq.com>
2021-12-20 19:24:46 +0800

    mysql

---
+++ zucker-easy-orm-rdb/src/main/java/org/zucker/ezorm/rdb/supports/mysql/MysqlPaginator.java
@@ -0,0 +1,27 @@
+package org.zucker.ezorm.rdb.supports.mysql;
+
+import org.zucker.ezorm.rdb.operator.builder.FragmentBlock;
+import org.zucker.ezorm.rdb.operator.builder.Paginator;
+import org.zucker.ezorm.rdb.operator.builder.fragments.BlockSqlFragments;
+import org.zucker.ezorm.rdb.operator.builder.fragments.PrepareSqlFragments;
+import org.zucker.ezorm.rdb.operator.builder.fragments.SqlFragments;
+
+/**
+ * @auther: lind
+ * @since: 1.0
+ */
+public class MysqlPaginator implements Paginator {
+
+    @Override
+    public SqlFragments doPaging(SqlFragments fragments, int pageIndex, int pageSize) {
+        if (fragments instanceof PrepareSqlFragments) {
+            ((PrepareSqlFragments) fragments).addSql("limit ?,?")
+                    .addParameter(pageIndex * pageSize, pageSize);
+        } else if (fragments instanceof BlockSqlFragments) {
+            ((BlockSqlFragments) fragments).addBlock(FragmentBlock.after, PrepareSqlFragments.of()
+                    .addSql("limit ?,?")
+                    .addParameter(pageIndex * pageSize, pageSize));
+        }
+        return fragments;
+    }
+}
---
+++ zucker-easy-orm-rdb/src/main/java/org/zucker/ezorm/rdb/supports/mysql/MysqlR2DBCExceptionTranslation.java
@@ -0,0 +1,26 @@
+package org.zucker.ezorm.rdb.supports.mysql;
+
+import io.r2dbc.spi.R2dbcException;
+import lombok.AllArgsConstructor;
+import org.zucker.ezorm.rdb.exception.DuplicateKeyException;
+import org.zucker.ezorm.rdb.operator.ExceptionTranslation;
+
+import java.util.Collections;
+
+/**
+ * @auther: lind
+ * @since: 1.0
+ */
+@AllArgsConstructor(staticName = "of")
+public class MysqlR2DBCExceptionTranslation implements ExceptionTranslation {
+    @Override
+    public Throwable translate(Throwable e) {
+        if (e instanceof R2dbcException) {
+            R2dbcException exception = (R2dbcException) e;
+            if (exception.getErrorCode() == 1062 || exception.getErrorCode() == 1022) {
+                throw new DuplicateKeyException(true, Collections.emptyList(), e);
+            }
+        }
+        return e;
+    }
+}
