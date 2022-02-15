1c84edbebfd68630ec3e6c2bc180ea7e79c758bc
Lind <448929489@qq.com>
2022-02-15 22:39:26 +0800

    MapResultWrapperTest

---
+++ zucker-easy-orm-rdb/src/test/java/org/zucker/ezorm/rdb/executor/wrapper/MapResultWrapperTest.java
@@ -0,0 +1,27 @@
+package org.zucker.ezorm.rdb.executor.wrapper;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Map;
+
+/**
+ * @auther: lind
+ * @since: 1.0
+ */
+public class MapResultWrapperTest {
+
+    @Test
+    public void testWrapper() {
+
+        MapResultWrapper wrapper = MapResultWrapper.defaultInstance();
+        Map<String, Object> instance = wrapper.newRowInstance();
+
+        wrapper.doWrap(instance, "id", 1);
+        wrapper.doWrap(instance, "info.name", "张三");
+        Assert.assertEquals(instance.get("id"), 1);
+        Assert.assertTrue(instance.get("info") instanceof Map);
+        Assert.assertEquals(((Map<?, ?>) instance.get("info")).get("name"), "张三");
+
+    }
+}
