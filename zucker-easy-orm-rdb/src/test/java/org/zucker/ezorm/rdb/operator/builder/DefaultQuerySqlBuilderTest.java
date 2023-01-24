aca2960d767377c35dedd8d5119b8e6267309092
lind <448929489@qq.com>
2023-01-24 20:00:37 +0800

    DefaultQuerySqlBuilderTest

---
+++ zucker-easy-orm-rdb/src/test/java/org/zucker/ezorm/rdb/operator/builder/DefaultQuerySqlBuilderTest.java
@@ -0,0 +1,12 @@
+package org.zucker.ezorm.rdb.operator.builder;
+
+import org.zucker.ezorm.rdb.metadata.RDBSchemaMetadata;
+
+public class DefaultQuerySqlBuilderTest {
+
+    RDBSchemaMetadata schema;
+
+    public void init(){
+
+    }
+}
