4af9340fc0eba44e5f05c22a85317df346dc2d40
lind <448929489@qq.com>
2023-01-27 16:58:04 +0800

    DefaultQuerySqlBuilderTest

--- zucker-easy-orm-rdb/src/test/java/org/zucker/ezorm/rdb/operator/builder/DefaultQuerySqlBuilderTest.java
+++ zucker-easy-orm-rdb/src/test/java/org/zucker/ezorm/rdb/operator/builder/DefaultQuerySqlBuilderTest.java
@@ -1,12 +1,48 @@
 package org.zucker.ezorm.rdb.operator.builder;
 
+import org.junit.Before;
+import org.zucker.ezorm.rdb.metadata.RDBColumnMetadata;
+import org.zucker.ezorm.rdb.metadata.RDBDatabaseMetadata;
 import org.zucker.ezorm.rdb.metadata.RDBSchemaMetadata;
+import org.zucker.ezorm.rdb.metadata.RDBTableMetadata;
+import org.zucker.ezorm.rdb.metadata.dialect.Dialect;
+
+import java.sql.JDBCType;
 
 public class DefaultQuerySqlBuilderTest {
 
     RDBSchemaMetadata schema;
 
-    public void init(){
+    @Before
+    public void init() {
+        RDBDatabaseMetadata database = new RDBDatabaseMetadata(Dialect.H2);
+        schema = new RDBSchemaMetadata("DEFAULT");
 
+        database.setCurrentSchema(schema);
+        database.addSchema(schema);
+
+        RDBTableMetadata table = new RDBTableMetadata();
+        table.setName("test");
+        RDBTableMetadata detail = new RDBTableMetadata();
+        detail.setName("detail");
+
+        schema.addTable(table);
+        schema.addTable(detail);
+
+        {
+            RDBColumnMetadata id = new RDBColumnMetadata();
+            id.setName("id");
+            id.setJdbcType(JDBCType.VARCHAR, String.class);
+            id.setLength(32);
+
+            RDBColumnMetadata name = new RDBColumnMetadata();
+            name.setName("name");
+            name.setJdbcType(JDBCType.VARCHAR, String.class);
+            name.setLength(64);
+
+            table.addColumn(id);
+            table.addColumn(name);
+
+        }
     }
 }
