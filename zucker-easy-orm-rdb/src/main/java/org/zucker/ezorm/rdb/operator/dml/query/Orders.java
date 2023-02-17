4ee7d085a6f9854ba473fd9bb5fe701eda20181a
lind <448929489@qq.com>
2023-02-17 23:32:21 +0800

    Orders

--- zucker-easy-orm-rdb/src/main/java/org/zucker/ezorm/rdb/operator/dml/query/Orders.java
+++ zucker-easy-orm-rdb/src/main/java/org/zucker/ezorm/rdb/operator/dml/query/Orders.java
@@ -2,6 +2,7 @@ package org.zucker.ezorm.rdb.operator.dml.query;
 
 import org.zucker.ezorm.rdb.operator.dml.FunctionColumn;
 import org.zucker.ezorm.rdb.operator.dml.Functions;
+import org.zucker.ezorm.rdb.operator.dml.SortOrderSupplier;
 
 import java.util.function.Supplier;
 
@@ -15,4 +16,15 @@ public interface Orders {
         return new OrderOperator(column.get());
     }
 
+    static OrderOperator count(String name) {
+        return function(Functions.count(name));
+    }
+
+    static SortOrderSupplier asc(String column) {
+        return new OrderOperator(column).asc();
+    }
+
+    static SortOrderSupplier desc(String column) {
+        return new OrderOperator(column).desc();
+    }
 }
